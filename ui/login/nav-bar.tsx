3c9e7a2c90f846ebe3c49e4e8a5257c85f00f8d0
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-20 13:24:31 -0300

    refactor(navBar): mapped all possible routes to links

--- ui/login/nav-bar.tsx
+++ ui/login/nav-bar.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import Link from 'next/link'
 export default function NavBar() {
+    const routes = [
+    {
+        href: '/login',
+        text: 'Login main'
+    },
+    {
+        href: '/login/user',
+        text: 'User'
+    },
+    {
+        href: '/login/admin',
+        text: 'Admin'
+    }
+    ]
   return (
     <div>
         <ul>
-            <li>
-                <Link href='/login'>Login main</Link>
-            </li>
-            <li>
-                <Link href='/login/user'>User</Link>
-            </li>
-            <li>
-                <Link href='/login/admin'>Admin</Link>
-            </li>
+            {routes?.map((route, key) => {
+                const { href, text } = route;
+                return (
+                    <li key={key}>
+                        <Link href={href}>{text}</Link>
+                    </li>
+                )
+            })}
         </ul>
     </div>
   )
