a3b92a7fae24996879c0ee702d1012c8773b1f95
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-20 17:47:05 -0300

    feat(seo): added metadata to login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,3 +5,10 @@ export default function Login() {
     <div>Login</div>
   )
 }
+
+export const generateMetadata = () => {
+  return {
+    title: 'Login page',
+    description: 'Users can login through this page'
+  }
+}
