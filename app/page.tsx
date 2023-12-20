814527ef89fbe087ddaa58a29e9a6f9ea9cfe2ba
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-20 10:59:53 -0300

    refactor(navigation): switched to useRouter hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link';
+
 
 const Home = () => {
+  const router = useRouter();
+
+  const navigate = (page: string): void => {
+    router.push(page);
+  }
   return (
-    <div>Home</div>
+    <section>
+      <h1>useRouter</h1>
+      <Link href="/projects">Projects Page</Link> <br />
+      <button className='border px-2 py-4' onClick={() => navigate("about")}>About Page</button> <br />
+      <button className='border px-2 py-4' onClick={() => navigate("login")}>Login Page</button>
+    </section>
   )
 }
 
