e58920964df3dcb82c6bec1a5a700bafcf149aac
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-21 12:05:02 -0300

    feat(images&fonts): added examples of both

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link';
+import { Roboto } from 'next/font/google';
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '500' 
+})
 
 const Home = () => {
   const router = useRouter();
@@ -13,6 +18,7 @@ const Home = () => {
   return (
     <section>
       <h1>useRouter</h1>
+      <h2 className={roboto.className}>This is the Home Page, in Roboto font</h2>
       <Link href="/projects">Projects Page</Link> <br />
       <button className='border px-2 py-4' onClick={() => navigate("about")}>About Page</button> <br />
       <button className='border px-2 py-4' onClick={() => navigate("login")}>Login Page</button>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Image from 'next/image';
+import beachImage from '@/public/rowan-heuvel-U6t80TWJ1DM-unsplash.jpg';
 
 export default function Users() {
   return (
-    <div>Users</div>
+    <div>
+      Users
+      <Image src={beachImage} alt='Beach Image' width={500} />
+      <p>
+        Foto de <br/>
+        <a href="https://unsplash.com/es/@insolitus?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Rowan Heuvel</a> <br/>
+        en <a href="https://unsplash.com/es/fotos/fotografia-de-paisaje-de-la-orilla-del-mar-bajo-nubes-cumulos-U6t80TWJ1DM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      </p>
+      <Image src='https://images.unsplash.com/photo-1703146173974-1a52b86f715d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='beach image' width={500} height={300} />
+    </div>
   )
 }
