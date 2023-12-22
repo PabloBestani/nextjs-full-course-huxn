53067930c2571322e3d9df651aef0570adbdefa8
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-22 14:13:03 -0300

    feat(users): added create user route&form

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    if (request.nextUrl.pathname !== '/login') {
-        return NextResponse.redirect(new URL('/login', request.url));
-    }
+    // if (request.nextUrl.pathname !== '/login') {
+    //     return NextResponse.redirect(new URL('/login', request.url));
+    // }
 
     // return NextResponse.redirect(new URL('/login', request.url));
-    return NextResponse.json({ success: 'ran successfully' });
+    // return NextResponse.json({ success: 'ran successfully' });
 }
 
 export const config = {
